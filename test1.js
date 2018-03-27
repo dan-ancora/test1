50f8ba992c6e3f2dd943572347135807e7dbe538
realtiti13 <30518006+realtiti13@users.noreply.github.com>
2018-03-27 19:00:09 +0300

    Update test1.js

--- test1.js
+++ test1.js
@@ -6,10 +6,16 @@ alert('Ana are mere');
 <h2>My First JavaScript</h2>
 
 <button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
+onclick="days+' '+document.getElementById('demo').innerHTML = Date()">
 Unde suntem in timp?.</button>
 
 <p id="demo"></p>
+
+<script>
+var d = new Date();
+var days = ["Dumineca","Lunie","Martz","Miercure","Jhoie","Viniere","Sambata"];
+document.getElementById("demo").innerHTML = days[d.getDay()];
+</script>
 
 </body>
 </html> 
